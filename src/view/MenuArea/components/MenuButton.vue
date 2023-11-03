7deccb38a7b200e8a7979359df0d85dc5edb4f76
jacky <jackywu526@gmail.com>
2023-11-03 17:21:54 +0800

    feat(MenuButton.vue): 建立 按鈕的組件

---
+++ src/view/MenuArea/components/MenuButton.vue
@@ -0,0 +1,51 @@
+<template>
+  <div
+    class="h-full w-1/3 cursor-pointer flex flex-col justify-evenly items-center pt-2 pb-2"
+    :class="bgColor"
+  >
+    <img
+      class=" h-1/2 w-auto"
+      :src="button.image"
+      @click="handleClick(button.name)"
+    >
+    <div class=" font-extrabold text-4xl mt-5 text-gray-50">
+      {{ button.name }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps({
+  button: {
+    type: Object,
+    required: true
+  },
+  index: {
+    type: Number,
+    required: true
+  },
+  activeButton: {
+    type: String,
+    required: true
+  }
+});
+
+const emit = defineEmits(["menuOpen"]);
+
+const bgColor = computed(() => {
+  switch (props.index) {
+    case 0:
+      return "bg-orange-400";
+    case 1:
+      return "bg-blue-400";
+    default:
+      return "bg-red-400";
+  }
+});
+
+const handleClick = (buttonName: string) => {
+  emit("menuOpen", buttonName);
+};
+</script>
