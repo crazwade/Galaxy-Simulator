80708527252e284d7c87689eff98446375cedeec
jacky <jackywu526@gmail.com>
2023-11-03 17:24:39 +0800

    feat(App.vue): 主畫面製作

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
+<template>
+  <div class="flex flex-col justify-center items-center w-full h-full">
+    <MainArea
+      class="h-[85vh] w-full bg-cover"
+      style="background-image: url('src/assets/background/spaceBG.jpg');"
+    />
+    <MenuArea
+      class="h-[15vh] w-full bg-emerald-100"
+      style="background-image: linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%);"
+      :menu="store.menu"
+      :visable="store.menuAreaVisable"
+      @menuOpen="menuClickEvent"
+    />
+  </div>
+</template>
+
 <script setup lang="ts">
-import MenuArea from "./view/MenuArea.vue";
+import MenuArea from "./view/MenuArea/MenuArea.vue";
 import MainArea from "./view/MainArea.vue";
 import { usePlanetStore } from "@/store/store.ts";
 
 const store = usePlanetStore();
-</script>
 
-<template>
-  <div class="flex flex-col justify-center items-center w-full h-full">
-    <MainArea class="h-[85vh] w-full bg-orange-100" />
-    <MenuArea class="h-[15vh] w-full bg-emerald-100" />
-  </div>
-</template>
+const menuClickEvent = () => {
+  store.menuAreaVisable = !store.menuAreaVisable;
+};
+</script>
 
 <style scoped>
 </style>
